cad6e1489afe80a6f97e4cc337ea2f4839bafe76
karvozavr <karvozavr@gmail.com>
2020-03-29 18:19:17 +0300

    Implement OldStorage

--- src/main/java/storage/OldStorage.java
+++ src/main/java/storage/OldStorage.java
@@ -1,4 +1,11 @@
 package storage;
 
-public class OldStorage {
+public class OldStorage extends Storage {
+
+    private final String ENDPOINT = System.getProperty("BASE_URL") + System.getProperty("OLD_STORAGE_ENDPOINT");
+
+    @Override
+    public String getEndpoint() {
+        return ENDPOINT;
+    }
 }
